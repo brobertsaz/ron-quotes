894347b69a4ae41ec817308f2fa2a786baf80f87
Bob Roberts <Bob@politiscope.io>
2019-05-03 18:40:50 -0500

    Add word count on save of quote

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,12 @@ class Quote < ApplicationRecord
   validates_presence_of :text
 
   has_many :quote_reviews
+
+  after_save :update_word_count
+
+  private
+
+  def update_word_count
+    self.update_columns(word_count: text.split.length)
+  end
 end
--- spec/models/quote_spec.rb
+++ spec/models/quote_spec.rb
@@ -4,12 +4,24 @@ RSpec.describe Quote, type: :model do
   describe 'requires text' do
     it 'fails with no text' do
       quote = Quote.create
-      expect(quote.save).to eq false
+      expect(quote.save).to be false
     end
 
     it 'succeeds with text' do
       quote = Quote.create text: Faker::TvShows::SouthPark.quote
-      expect(quote.save).to eq true
+      expect(quote.save).to be true
+    end
+  end
+
+  describe 'word count' do
+    let!(:small)   { Quote.create(text: Faker::Lorem.sentence(4)) }
+    let!(:medium)  { Quote.create(text: Faker::Lorem.sentence(7)) }
+    let!(:large)   { Quote.create(text: Faker::Lorem.sentence(13)) }
+
+    it 'returns the number of words in text' do
+      expect(small.word_count).to eq 4
+      expect(medium.word_count).to eq 7
+      expect(large.word_count).to eq 13
     end
   end
 end
